7b0eee862a5a079d635517e66630354e6815e315
altus34 <d.avenante@gmail.com>
2014-12-25 21:55:46 -0500

    Add doc link

--- js/testatoo.js
+++ js/testatoo.js
@@ -3,9 +3,26 @@
     $("header").load("template/header.html", function() {
         var section = $('section').attr('id');
         $('[role= ' + section + ']').addClass('active');
-
     });
 
     $("footer").load("template/footer.html");
 
+    window.setIframeHeight = function(iframe) {
+        if (iframe) {
+            var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
+            if (iframeWin.document.body) {
+                iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight + 500;
+            }
+        }
+    };
+
+
+
+
 })($);
+
+
+$(window).load(function () {
+    window.setIframeHeight(document.getElementById('doc'));
+});
+
