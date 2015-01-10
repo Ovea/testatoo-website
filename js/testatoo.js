8bb43bbbb9a73cbd4a83d745a788586370de8343
altus34 <d.avenante@gmail.com>
2015-01-10 13:38:30 -0500

    Update to testatoo 2.0.b3 release

--- js/testatoo.js
+++ js/testatoo.js
@@ -7,19 +7,4 @@
 
     $("footer").load("template/footer.html");
 
-    window.setIframeHeight = function(iframe) {
-        if (iframe) {
-            var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
-            if (iframeWin.document.body) {
-                iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight + 500;
-            }
-        }
-    };
-
 })($);
-
-
-$(window).load(function () {
-    window.setIframeHeight(document.getElementById('doc'));
-});
-
